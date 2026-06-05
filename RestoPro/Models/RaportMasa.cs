ba7ce337c5216d3e55c09a728e13ee5812d5b7e2
SibaevVasile <157913814+SibaevVasile@users.noreply.github.com>
2026-06-06 00:14:25 +0300

    feat: implement RaportWindow with summary DataGrid #11

---
+++ RestoPro/Models/RaportMasa.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestoPro.Models
+{
+    public class RaportMasa
+    {
+        public int NumarMasa { get; set; }
+        public string Zona { get; set; }
+        public int NrComenzi { get; set; }
+        public decimal TotalAchitat { get; set; }
+    }
+}
