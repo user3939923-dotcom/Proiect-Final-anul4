c9e55e3588f14ecc80883e230cd972fd82166a92
SibaevVasile <157913814+SibaevVasile@users.noreply.github.com>
2026-06-05 19:14:06 +0300

    feat: implement DatabaseHelper with GetConnection method #4

--- RestoPro/Data/DatabaseHelper.cs
+++ RestoPro/Data/DatabaseHelper.cs
@@ -1,18 +1,38 @@
 ﻿using Microsoft.Data.SqlClient;
-using Microsoft.IdentityModel.Protocols;
 using System.Configuration;
+using System.Windows;
 
 namespace RestoPro.Data
 {
     public class DatabaseHelper
     {
-        private static string _connectionString =
+        private static readonly string _connectionString =
             ConfigurationManager.ConnectionStrings["RestoPro"]
             .ConnectionString;
 
         public static SqlConnection GetConnection()
         {
             return new SqlConnection(_connectionString);
+        }
+        public static bool TestConnection()
+        {
+            try
+            {
+                using (var conn = GetConnection())
+                {
+                    conn.Open();
+                    return true;
+                }
+            }
+            catch (SqlException ex)
+            {
+                MessageBox.Show(
+                    $"Eroare conexiune BD:\n{ex.Message}",
+                    "Eroare",
+                    MessageBoxButton.OK,
+                    MessageBoxImage.Error);
+                return false;
+            }
         }
     }
 }
