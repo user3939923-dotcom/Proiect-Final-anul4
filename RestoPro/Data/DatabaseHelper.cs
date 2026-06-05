42788c35be4caa0df2d871478549785e99d16916
SibaevVasile <157913814+SibaevVasile@users.noreply.github.com>
2026-06-05 20:15:08 +0300

    feat: add AddMasa, UpdateMasa, DeleteMasa to DatabaseHelper #7

--- RestoPro/Data/DatabaseHelper.cs
+++ RestoPro/Data/DatabaseHelper.cs
@@ -88,5 +88,71 @@ namespace RestoPro.Data
             }
             return list;
         }
+        public static void AddMasa(Masa m)
+        {
+            using (var conn = GetConnection())
+            {
+                conn.Open();
+                var cmd = new SqlCommand(
+                    "INSERT INTO Masa (NumarMasa, Capacitate, Zona) " +
+                    "VALUES (@n, @c, @z)", conn);
+                cmd.Parameters.AddWithValue("@n", m.NumarMasa);
+                cmd.Parameters.AddWithValue("@c", m.Capacitate);
+                cmd.Parameters.AddWithValue("@z", m.Zona);
+                cmd.ExecuteNonQuery();
+            }
+        }
+
+        public static void UpdateMasa(Masa m)
+        {
+            using (var conn = GetConnection())
+            {
+                conn.Open();
+                var cmd = new SqlCommand(
+                    "UPDATE Masa SET NumarMasa=@n, Capacitate=@c, " +
+                    "Zona=@z WHERE IdMasa=@id", conn);
+                cmd.Parameters.AddWithValue("@n", m.NumarMasa);
+                cmd.Parameters.AddWithValue("@c", m.Capacitate);
+                cmd.Parameters.AddWithValue("@z", m.Zona);
+                cmd.Parameters.AddWithValue("@id", m.IdMasa);
+                cmd.ExecuteNonQuery();
+            }
+        }
+
+        public static void DeleteMasa(int id)
+        {
+            using (var conn = GetConnection())
+            {
+                conn.Open();
+                var cmd = new SqlCommand(
+                    "DELETE FROM Masa WHERE IdMasa=@id", conn);
+                cmd.Parameters.AddWithValue("@id", id);
+                cmd.ExecuteNonQuery();
+            }
+        }
+
+        public static List<Masa> SearchMese(string search)
+        {
+            var list = new List<Masa>();
+            using (var conn = GetConnection())
+            {
+                conn.Open();
+                var cmd = new SqlCommand(
+                    "SELECT IdMasa, NumarMasa, Capacitate, Zona " +
+                    "FROM Masa WHERE Zona LIKE @s OR " +
+                    "CAST(NumarMasa AS NVARCHAR) LIKE @s", conn);
+                cmd.Parameters.AddWithValue("@s", $"%{search}%");
+                using (var reader = cmd.ExecuteReader())
+                    while (reader.Read())
+                        list.Add(new Masa
+                        {
+                            IdMasa = reader.GetInt32(0),
+                            NumarMasa = reader.GetInt32(1),
+                            Capacitate = reader.GetInt32(2),
+                            Zona = reader.GetString(3)
+                        });
+            }
+            return list;
+        }
     }
 }
