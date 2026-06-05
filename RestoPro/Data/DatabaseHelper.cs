f87e79de887ec5bd61d91c8a68de6a0e51a02c0a
SibaevVasile <157913814+SibaevVasile@users.noreply.github.com>
2026-06-05 19:40:14 +0300

    feat: add GetAllMese and GetAllProduse to DatabaseHelper

--- RestoPro/Data/DatabaseHelper.cs
+++ RestoPro/Data/DatabaseHelper.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.Data.SqlClient;
+using RestoPro.Models;
+using System.Collections.Generic;
 using System.Configuration;
 using System.Windows;
 
@@ -33,6 +35,58 @@ namespace RestoPro.Data
                     MessageBoxImage.Error);
                 return false;
             }
+        }
+        public static List<Masa> GetAllMese()
+        {
+            var list = new List<Masa>();
+            using (var conn = GetConnection())
+            {
+                conn.Open();
+                var cmd = new SqlCommand(
+                    "SELECT IdMasa, NumarMasa, Capacitate, Zona FROM Masa",
+                    conn);
+                using (var reader = cmd.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        list.Add(new Masa
+                        {
+                            IdMasa = reader.GetInt32(0),
+                            NumarMasa = reader.GetInt32(1),
+                            Capacitate = reader.GetInt32(2),
+                            Zona = reader.GetString(3)
+                        });
+                    }
+                }
+            }
+            return list;
+        }
+
+        public static List<Produs> GetAllProduse()
+        {
+            var list = new List<Produs>();
+            using (var conn = GetConnection())
+            {
+                conn.Open();
+                var cmd = new SqlCommand(
+                    "SELECT IdProdus, Denumire, Categorie, Pret, " +
+                    "Disponibil FROM Produs", conn);
+                using (var reader = cmd.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        list.Add(new Produs
+                        {
+                            IdProdus = reader.GetInt32(0),
+                            Denumire = reader.GetString(1),
+                            Categorie = reader.GetString(2),
+                            Pret = reader.GetDecimal(3),
+                            Disponibil = reader.GetBoolean(4)
+                        });
+                    }
+                }
+            }
+            return list;
         }
     }
 }
