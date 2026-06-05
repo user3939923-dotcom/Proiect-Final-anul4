084bbe32700fcbf3e8040ed8ab72393758d40557
SibaevVasile <157913814+SibaevVasile@users.noreply.github.com>
2026-06-05 19:41:32 +0300

    feat: display Masa data in DataGrid as connection test #6

---
+++ RestoPro/Views/MasePage.xaml.cs
@@ -0,0 +1,14 @@
+﻿using RestoPro.Data;
+using System.Windows.Controls;
+
+namespace RestoPro.Views
+{
+    public partial class MasePage : Page
+    {
+        public MasePage()
+        {
+            InitializeComponent();
+            DgMese.ItemsSource = DatabaseHelper.GetAllMese();
+        }
+    }
+}
